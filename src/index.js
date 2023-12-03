3c5bdd06a6f141b72aca731ec4f23b30b54cfa65
Karmveer <karmveer.nz@gmail.com>
2023-12-04 01:38:02 +1300

    Created basic webpack enviroment with Sass loaders and HTMLWebpackPlugin

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import name from "./testing"
+import './styles/index.scss'
+const nameStatement = name();
+console.log(nameStatement);
---
+++ src/testing.js
@@ -0,0 +1,4 @@
+function name(){
+    return "My name is Smith.";
+};
+export default  name;
