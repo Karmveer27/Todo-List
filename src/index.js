474168f2690b1f630713a6d8fa3b486cc258a2da
Karmveer <karmveer.nz@gmail.com>
2023-12-06 15:13:27 +1300

    Added checkmate logo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import name from "./modules/testing"
 import DateClass from "./modules/Date"
 import './styles/index.scss'
-import {ca} from "date-fns/locale";
 
+//Images below
+import inbox from './assets/inbox.png'
+import sevenDays from './assets/7days.png'
+import today from './assets/today.png'
+import checkmate from './assets/checkmate1.png'
+import add from './assets/add.png'
+import important from './assets/flag.png'
+import menuEntry from './assets/menu-entry.png'
+
+document.getElementById('inbox').src = inbox;
+document.getElementById('7days').src = sevenDays;
+document.getElementById('today').src = today;
+document.getElementById('logo').src = checkmate;
+document.getElementById('add-project').src = add;
+document.getElementById('important').src = important;
+document.getElementById('home').src = menuEntry;
 
 
 
