2809b7f77213fd2f7fd9c7ac7ae78d94770fb268
Karmveer <karmveer.nz@gmail.com>
2023-12-06 15:12:56 +1300

    Contunued working on HTML page, nav options and logos are present

--- src/index.js
+++ src/index.js
@@ -2,21 +2,11 @@ import name from "./modules/testing"
 import DateClass from "./modules/Date"
 import './styles/index.scss'
 import {ca} from "date-fns/locale";
-const nameStatement = name();
-//console.log(nameStatement);
 
-const DateInstance = new DateClass();
-DateInstance.getCurrentTime()
-//console.log(DateInstance.getMonth(1701768545258))
 
-const calender = document.getElementById("myDate");
-const button = document.getElementById("dateButton");
-button.addEventListener('click',(e)=>{
-    e.preventDefault();
-    console.log("getting here");
-    console.log(calender.value);
-    console.log(typeof(calender.value));
-})
+
+
+
 
 
 
